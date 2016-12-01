1dfed871f87dd2c2acbd7887b4f526eb54723e01
Baptiste BOUCHEREAU <baptiste.bouchereau@idci-consulting.fr>
2016-12-01 11:31:05 +0100

    Add the bundle class

---
+++ IDCIWebsiteConfiguratorBundle.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * @author:  Baptiste BOUCHEREAU <baptiste.bouchereau@idci-consulting.fr>
+ * @license: MIT
+ */
+
+namespace IDCI\Bundle\WebsiteConfiguratorBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+
+class IDCIWebsiteConfiguratorBundle extends Bundle
+{
+    public function build(ContainerBuilder $container)
+    {
+
+    }
+}
